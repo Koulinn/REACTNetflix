0809ef6f801a25f0e6d93a5a46ad17dd1ad4b56f
Koulinn <drdverzola@gmail.com>
2021-07-14 14:07:24 +0100

    Request working
    Also created isLoading useState

--- netflix/src/components/Details.jsx
+++ netflix/src/components/Details.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
+import {Container} from 'react-bootstrap'
 
 export default function Details(props) {
-    console.log(props, props.match.params.movieId)
+    const [movie, setMovie] = useState(null)
+    const [notFound, setNotFound] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    
+
+    useEffect(()=>{
+        getMovieDetails()
+    }, [])
+    
+    const getMovieDetails = async() =>{
+        try {
+            
+            let resp = await fetch('http://www.omdbapi.com/?apikey=1aad5b7f&i=' + props.match.params.movieId)
+    
+            let moviesResp = await resp.json()
+            console.log(moviesResp, "<<<<<<<<<<<details request")
+        } catch (error) {
+            
+        }
+
+    }
+
+
     return (
-        <div>
+        <Container>
             <h1>Details Page</h1>
-        </div>
+        </Container>
     )
 }
